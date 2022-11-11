bafc15debef78aad60b6e113707f93d228e4746f
Safi <safi.sharieff@gmail.com>
2022-11-11 21:45:03 +0400

    Added more elements to the topbar

--- stock_shadower/src/components/Topbar/Topbar.js
+++ stock_shadower/src/components/Topbar/Topbar.js
@@ -3,7 +3,11 @@ import '../Container/Container.css'
 function Mainbody(){
     return(
         <div className="primus_in_primo">
-
+            <div>Stock Shadower</div>
+            <div>Add</div>
+            <div>View</div>
+            <div>About</div>
+            <div><img></img></div>
         </div>
     )
 }
