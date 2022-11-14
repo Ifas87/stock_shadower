3ce68a84f66d056b67b08e8b87f8444ba5fc7a28
Safi <safi.sharieff@gmail.com>
2022-11-14 18:48:15 +0400

    Added more styling and readjustments

--- stock_shadower/src/components/Topbar/Topbar.js
+++ stock_shadower/src/components/Topbar/Topbar.js
@@ -1,13 +1,14 @@
 import '../Container/Container.css'
+import BG from '../Container/BG_holder2.png'
 
 function Mainbody(){
     return(
         <div className="primus_in_primo">
-            <div className='title_fonts standard_box'>Stock Shadower</div>
-            <div className='sub_title_fonts standard_box'>Add</div>
-            <div className='sub_title_fonts standard_box'>View</div>
-            <div className='sub_title_fonts standard_box'>About</div>
-            <div className='standard_box'><img></img></div>
+            <div className='title_fonts standard_box quinque'>Stock Shadower</div>
+            <div className='sub_title_fonts standard_box unus'>Add</div>
+            <div className='sub_title_fonts standard_box unus'>View</div>
+            <div className='sub_title_fonts standard_box unus'>About</div>
+            <div className='standard_box duo'><img src={BG} width={50} height={50}></img></div>
         </div>
     )
 }
