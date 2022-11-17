89965425b4b510610c4df5b6ec89d8272118a2db
Safi <safi.sharieff@gmail.com>
2022-11-17 18:49:52 +0400

    Added more styles and layout to the new component

--- stock_shadower/src/components/StockBox/StockBox.js
+++ stock_shadower/src/components/StockBox/StockBox.js
@@ -1,7 +1,15 @@
 import '../Container/Container.css';
 
 function StockBox(){
-
+    return(
+        <div className='stockbox'>
+            <div>
+                <div className='Ticker'></div>
+                <div className='Comp_name'></div>
+            </div>
+            <div className='Value'></div>
+        </div>
+    );
 }
 
 export default StockBox;
