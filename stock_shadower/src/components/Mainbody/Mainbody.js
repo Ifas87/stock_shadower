3ea027eee2a87ab7fd834c3fe00caf062c916300
Safi <safi.sharieff@gmail.com>
2022-11-20 19:46:29 +0400

    Working on the logic of the project

--- stock_shadower/src/components/Mainbody/Mainbody.js
+++ stock_shadower/src/components/Mainbody/Mainbody.js
@@ -1,9 +1,10 @@
 import '../Container/Container.css'
+import StockBox from '../StockBox/StockBox';
 
 function Mainbody(){
     return(
         <div className="secundo_in_primo">
-
+            <StockBox tick="GOEV" name="GOOGLE" value="99"></StockBox>
         </div>
     )
 }
