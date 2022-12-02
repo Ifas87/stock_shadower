eeb4a2b7c6aa5b2ae2b51507b359b625db9459cf
Safi <safi.sharieff@gmail.com>
2022-12-02 20:36:34 +0400

    Minor changes

--- stock_shadower/src/components/StockBox/StockBox.js
+++ stock_shadower/src/components/StockBox/StockBox.js
@@ -3,7 +3,7 @@ import '../Container/Container.css';
 function StockBox(props){
     return(
         <div className='stockbox'>
-            <div style={{margin: "30px"}}>
+            <div style={{margin: "30px"}}> 
                 <div className='Ticker'>{props.tick}</div>
                 <div className='Comp_name'>{props.name}</div>
             </div>
