a83520007c2c2116cab6c83b454efc966c51f6c9
Safi <safi.sharieff@gmail.com>
2022-12-06 18:20:06 +0400

    Added some URL routing

--- stock_shadower/src/App.js
+++ stock_shadower/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 import Container from "./components/Container/Container"
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Adder from './components/Adder/Adder';
 
 function App() {
   return (
-    <Container></Container>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Container></Container>
+        </Route>
+
+        <Route path="/add">
+          <Adder></Adder>
+        </Route>
+
+        <Route>
+          
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
   );
 }
 
